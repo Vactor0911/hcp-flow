7a95dd1d48158d1d5dd432622bb945c7e8e138e3
Vactor0911 <vactor0911@gmail.com>
2024-10-24 23:22:31 +0900

    Added basic frames

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,28 @@
+import styled from "@emotion/styled";
+
 function App() {
+  const Style = styled.div`
+    display: flex;
+    height: 100vh;
+  `;
+  const SideMenu = styled.div`
+    width: 20vw;
+    max-width: 300px;
+    background-color: #f0f0f0;
+  `;
+  const Content = styled.div`
+    flex: 1;
+  `;
+
   return (
-    <>
-      <h1>Hello World!</h1>
-      <p>This is a test message.</p>
-    </>
+    <Style>
+      <SideMenu>
+        <h1>Side Menu</h1>
+      </SideMenu>
+      <Content>
+        <h1>Content</h1>
+      </Content>
+    </Style>
   );
 }
 
